7404e3cd3bad3abbe581fc294fc32e5161577c0a
Shion <shion24stat@gmail.com>
2022-10-19 19:20:33 +0900

    calculation of ROA for 4 periods

--- roarecalc/main.py
+++ roarecalc/main.py
@@ -14,6 +14,7 @@ import os
 import string
 import logging
 import sqlite3
+import time
 
 import minkabu
 
@@ -28,15 +29,33 @@ logging.basicConfig(
 def printLog(
     dataProcessed : int,
     companyName : str,
-    netIncome : float,
-    totalAssets : float,
-    roa : float
+    netIncome_0 : float,
+    totalAssets_0 : float,
+    netIncome_1 : float,
+    totalAssets_1 : float,
+    netIncome_2 : float,
+    totalAssets_2 : float,
+    netIncome_3 : float,
+    totalAssets_3 : float,
+    roa_0 : float,
+    roa_1 : float,
+    roa_2 : float,
+    roa_3 : float,
 ) -> None:
-    logging.info("data processed : No.{}".format(dataProcessed))
+    logging.info("\ndata processed : No.{}".format(dataProcessed))
     logging.info("company name : {}".format(companyName))
-    logging.info("net income : {}(yen)".format(netIncome))
-    logging.info("total assets : {}(yen)".format(totalAssets))
-    logging.info("ROA : {}%".format(roa))
+    logging.info("net income-0 : {} (yen)".format(netIncome_0))
+    logging.info("total assets-0 : {} (yen)".format(totalAssets_0))
+    logging.info("net income-1 : {} (yen)".format(netIncome_1))
+    logging.info("total assets-1 : {} (yen)".format(totalAssets_1))
+    logging.info("net income-2 : {} (yen)".format(netIncome_2))
+    logging.info("total assets-2 : {} (yen)".format(totalAssets_2))
+    logging.info("net income-3 : {} (yen)".format(netIncome_3))
+    logging.info("total assets-3 : {} (yen)".format(totalAssets_3))
+    logging.info("ROA-0 : {} (%)".format(roa_0))
+    logging.info("ROA-1 : {} (%)".format(roa_1))
+    logging.info("ROA-2 : {} (%)".format(roa_2))
+    logging.info("ROA-3 : {} (%)\n".format(roa_3))
 
 #class for openpyxl processing
 class Exproc :
@@ -136,12 +155,21 @@ if __name__ == "__main__" :
     #for excel
     exproc = Exproc("./data/roacalc.xlsx")
     exproc.prepare(
-        headers=["COMPANY NAME", "NET INCOME", "TOTAL ASSETS", "ROA"]
-    )
-    message = "source code : https://github.com/shion24hub/ROA-recalc"
-    exproc.write(
-        "F1",
-        message,
+        headers=[
+            "COMPANY NAME",
+            "NET INCOME-0 (yen)",
+            "TOTAL ASSETS-0 (yen)",
+            "NET INCOME-1 (yen)",
+            "TOTAL ASSETS-1 (yen)",
+            "NET INCOME-2 (yen)",
+            "TOTAL ASSETS-2 (yen)",
+            "NET INCOME-3 (yen)",
+            "TOTAL ASSETS-3 (yen)",
+            "ROA-0 (%)",
+            "ROA-1 (%)",
+            "ROA-2 (%)",
+            "ROA-3 (%)"
+        ]
     )
 
     #for database
@@ -159,10 +187,10 @@ if __name__ == "__main__" :
         names.append(''.join(name.split()))
     
     if DEBUG_MODE :
-        names = names[:20]
+        names = names[:5]
 
     nameCodeDict = {}
-    for name in names :
+    for ind, name in enumerate(names) :
         res = minkabu.minkabu(name)
 
         if len(res) == 0 :
@@ -172,7 +200,14 @@ if __name__ == "__main__" :
         va = list(res.values())
         nameCodeDict[ky[0]] = va[0]
 
+        if ind % 50 == 0 :
+            print("\nThe system goes into a 10-second sleep.\n")
+            time.sleep(10)
+            print("\nSleep for 10 seconds has been completed. Processing will continue.\n")
+
     dataProcessed = 1
+    countError = 0
+    normalProcessing = 0
     for name, scode in nameCodeDict.items() :
         try : 
             scode += ".T"
@@ -188,34 +223,109 @@ if __name__ == "__main__" :
             insertInfo = [(name, fiJson, bsJson)]
             db.cursor.executemany(insertQuery, insertInfo)
 
-            netIncome = fi.loc["Net Income"][0]
-            totalAssets = bs.loc["Total Assets"][0]
-            roa = calcroa(netIncome, totalAssets)
+            netIncome_0 = fi.loc["Net Income"][0]
+            netIncome_1 = fi.loc["Net Income"][1]
+            netIncome_2 = fi.loc["Net Income"][2]
+            netIncome_3 = fi.loc["Net Income"][3]
+            totalAssets_0 = bs.loc["Total Assets"][0]
+            totalAssets_1 = bs.loc["Total Assets"][1]
+            totalAssets_2 = bs.loc["Total Assets"][2]
+            totalAssets_3 = bs.loc["Total Assets"][3]
+            roa_0 = calcroa(netIncome_0, totalAssets_0)
+            roa_1 = calcroa(netIncome_1, totalAssets_1)
+            roa_2 = calcroa(netIncome_2, totalAssets_2)
+            roa_3 = calcroa(netIncome_3, totalAssets_3)
             
             printLog(
                 dataProcessed,
                 name,
-                netIncome,
-                totalAssets,
-                roa
+                netIncome_0,
+                totalAssets_0,
+                netIncome_1,
+                totalAssets_1,
+                netIncome_2,
+                totalAssets_2,
+                netIncome_3,
+                totalAssets_3,
+                roa_0,
+                roa_1,
+                roa_2,
+                roa_3
             )
 
             exproc.insert(
-                values=[name, netIncome, totalAssets, roa]
+                values=[
+                    name, 
+                    netIncome_0,
+                    totalAssets_0,
+                    netIncome_1,
+                    totalAssets_1,
+                    netIncome_2,
+                    totalAssets_2,
+                    netIncome_3,
+                    totalAssets_3, 
+                    roa_0,
+                    roa_1,
+                    roa_2,
+                    roa_3
+                ]
             )
+            normalProcessing += 1
         except :
+            countError += 1
             printLog(
                 dataProcessed,
                 name,
-                netIncome,
-                totalAssets,
-                roa
+                netIncome_0,
+                totalAssets_0,
+                netIncome_1,
+                totalAssets_1,
+                netIncome_2,
+                totalAssets_2,
+                netIncome_3,
+                totalAssets_3,
+                roa_0,
+                roa_1,
+                roa_2,
+                roa_3
             )
+
+            values=[name]
+            for _ in range(12) :
+                values.append("ERROR")
             exproc.insert(
-                values=[name, "ERROR", "ERROR", "ERROR"]
+                values
             )
 
         dataProcessed += 1
+    
+    """
+    TODO :The following process will result in an error. The reason is under investigation.
+    """
+    # try :
+    #     message1 = "Total count : " + str(dataProcessed)
+    #     message2 = "Normal processing : " + str(normalProcessing)
+    #     message3 = "error count : " + str(countError)
+    #     message4 = "source code : https://github.com/shion24hub/ROA-recalc"
+    #     messages = [message1, message2, message3, message4]
+    #     for i in range(4) :
+    #         cell = "O" + str(i)
+    #         exproc.write(
+    #             cell,
+    #             messages[i]
+    #         )
+    # except :
+    #     pass
+    message1 = "Total count : " + str(dataProcessed - 1)
+    message2 = "Normal processing : " + str(normalProcessing)
+    message3 = "error count : " + str(countError)
+    message4 = "source code : https://github.com/shion24hub/ROA-recalc"
 
-db.finish()
-exproc.finish()
+    exproc.write("O1", message1)
+    exproc.write("O2", message2)
+    exproc.write("O3", message3)
+    exproc.write("O4", message4)
+
+    
+    db.finish()
+    exproc.finish()
